2fe188b423ff53f97b4a671e4dacb8e0081facf4
Allisa LeBeuf <al5906@gmail.com>
2018-10-11 21:36:29 -0700

    initial commit

--- Word Guess Game/Program.cs
+++ Word Guess Game/Program.cs
@@ -2,11 +2,11 @@
 
 namespace Word_Guess_Game
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello user, welcome to the Word Guessing Game!");
         }
     }
 }
