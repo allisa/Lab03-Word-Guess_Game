b1e9b1fe932b4a4d39d50e2f34bbdd523ed38028
Allisa LeBeuf <al5906@gmail.com>
2018-10-13 19:43:15 -0700

    adding words working

--- Word Guess Game/Program.cs
+++ Word Guess Game/Program.cs
@@ -5,19 +5,14 @@ namespace Word_Guess_Game
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello friend, welcome to the Word Guessing Game!");
 
             string path = "../../../gamewords.txt";
 
             CreateFile(path);
-            ReadFile(path);
-            //AppendToFile(path);
-            //DeleteLineFromFile(path);
-
-
-            //GameMenu();
+            GameMenu(path);
 
         }
 
@@ -25,48 +20,48 @@ namespace Word_Guess_Game
         /// <summary>
         /// Method for game menu, do behavior based on used input
         /// </summary>
-        //public static void GameMenu()
-        //{
-        //    bool exit = false;
-        //    while (!exit)
-        //    {
-        //        Console.WriteLine("Please select an option below:");
-        //        Console.WriteLine("0 - Home, 1 - Play, 2 - Word Bank, 3 - Add a Word, 4 - Delete a Word, 5 - Admin, 6 - Exit");
-        //        int optionSelection = Convert.ToInt32(Console.ReadLine());
+        public static void GameMenu(string path)
+        {
+            bool exit = false;
+            while (!exit)
+            {
+                Console.WriteLine("Please select an option below:");
+                Console.WriteLine("0 - Home, 1 - Play, 2 - Word Bank, 3 - Add a Word, 4 - Delete a Word, 5 - Admin, 6 - Exit");
+                int optionSelection = Convert.ToInt32(Console.ReadLine());
 
-        //        switch (optionSelection)
-        //        {
-        //            case 0:
-        //                GameMenu();
-        //                break;
-        //            case 1:
-        //                Play();
-        //                break;
-        //            case 2:
-        //                WordBank();
-        //                break;
-        //            case 3:
-        //                Console.WriteLine("Please enter a word to add to the game: ");
-        //                string wordToAdd = Console.ReadLine();
-        //                AddWord(wordToAdd);
-        //                break;
-        //            case 4:
-        //                Console.WriteLine("Please enter a word to delete from the game: ");
-        //                string wordToRemove = Console.ReadLine();
-        //                RemoveWord(wordToRemove);
-        //                break;
-        //            case 5:
-        //                Admin();
-        //                break;
-        //            case 6:
-        //                exit = true;
-        //                break;
-        //            default:
-        //                Console.WriteLine("Invalid option");
-        //                break;
-        //        }
-        //    }
-        //}
+                switch (optionSelection)
+                {
+                    case 0:
+                        GameMenu(path);
+                        break;
+                    case 1:
+                       // Play();
+                        break;
+                    case 2:
+                        ReadFile(path);
+                        break;
+                    case 3:
+                        Console.WriteLine("Please enter a word to add to the game: ");
+                        string wordToAdd = Console.ReadLine();
+                        AddWordToGame(path, wordToAdd);
+                        break;
+                    case 4:
+                        Console.WriteLine("Please enter a word to delete from the game: ");
+                        string wordToRemove = Console.ReadLine();
+                        RemoveWord(path, wordToRemove);
+                        break;
+                    case 5:
+                        //Admin();
+                        break;
+                    case 6:
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option");
+                        break;
+                }
+            }
+        }
 
         /// <summary>
         /// Method to create starter words in txt file
@@ -104,7 +99,7 @@ namespace Word_Guess_Game
         /// Method to take words from txt file and show them on the console app
         /// </summary>
         /// <param name="path"></param>
-        static void ReadFile(string path)
+        public static void ReadFile(string path)
         {
             try
             {
@@ -122,9 +117,70 @@ namespace Word_Guess_Game
             }
         }
 
+        /// <summary>
+        /// Method to add a word to the game after user selects to add word from game menu
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="wordToAdd"></param>
+        public static void AddWordToGame(string path, string wordToAdd)
+        {
 
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                try
+                {
+                    sw.WriteLine(wordToAdd);
+                    Console.WriteLine("Word has been added to the game");
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("An error has a found way into your life");
+                }
+            }
+        }
 
-
-
+        /// <summary>
+        /// Method to remove word entered by user after prompt from user menu
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="wordToRemove"></param>
+        public static void RemoveWord(string path, string wordToRemove)
+        {
+            try
+            {
+                string[] currentWords = ReadFile(path);
+                string[] newWords = new string[currentWords.Length - 1];
+                for (int i = 0; i < currentWords.Length; i++)
+                {
+                    if (wordToRemove != currentWords[i])
+                    {
+                        newWords[i] = currentWords[i];
+                    }
+                    //else
+                    //{
+                    //    Console.WriteLine($"The word you entered, {wordToRemove}, was not found");
+                    //}
+                }
+                try
+                {
+                    using (StreamWriter words = File.AppendText(path))
+                    {
+                        for(int i = 0; i < newWords.Length; i++)
+                        {
+                            words.WriteLine(newWords);
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                Console.WriteLine($"The word you requested to remove, {wordToRemove}, had been deleted.");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error");
+            }
+        }
     }
 }
